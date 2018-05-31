ee5d84a34dfebedadf9c28308008df962c30745b
Pattnom <pattnom@gmail.com>
2018-05-31 17:49:02 +0000

    3. Delegate Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -18,7 +18,17 @@ contract DappToken {
 		uint256 _value
 	);
 
+	// approve
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
+
 	mapping(address => uint256) public balanceOf;
+	// allowance
+	mapping(address => mapping(address => uint256)) public allowance;
+	
 
 	function DappToken (uint256 _initialSupply) public {
 		balanceOf[msg.sender] = _initialSupply;
@@ -36,6 +46,36 @@ contract DappToken {
 		// Transfer Event
 		Transfer(msg.sender, _to, _value);
 		// Return boolean
+		return true;
+	}
+	
+	// Approve
+	function approve(address _spender, uint256 _value) public returns (bool success){
+		// allowance
+		allowance[msg.sender][_spender] = _value;
+
+		// approve event
+		Approval(msg.sender, _spender, _value);
+
+		return true;
+	}
+
+	// TransferFrom
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+
+		// Require _from has enough tokens
+		require(_value <= balanceOf[_from]);
+		// Require allowance is big enough
+		require(_value <= allowance[_from][msg.sender]);
+		// Change the balance
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		// Update the allowance
+		allowance[_from][msg.sender] -= _value;
+		
+		// Transfer event
+		Transfer(_from, _to, _value);
+		// return a boolean
 		return true;
 	}
 }
